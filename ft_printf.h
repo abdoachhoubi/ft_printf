5f9d964248970f7f554a25b32ae624e24442477f
abdoachhoubi <abdo.achhoubi2@gmail.com>
2022-09-03 20:44:23 +0100

    Updated recently Fixed/Added functions

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: aachhoub <aachhoub@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/03 14:42:37 by aachhoub          #+#    #+#             */
-/*   Updated: 2022/09/03 14:53:04 by aachhoub         ###   ########.fr       */
+/*   Updated: 2022/09/03 20:03:23 by aachhoub         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,9 @@
 void	ft_putchar(char c, int *index);
 void	ft_putstr(const char *s, int *index);
 void	ft_putnbr(int nb, int *index);
+void	ft_putnbr_u(unsigned int nb, int *index);
 void	ft_putnbr_base(int nb, char cs, int *index);
+void	ft_print_memory(const void *ptr, int *index);
+int		ft_printf(const char *format, ...);
 
 #endif
