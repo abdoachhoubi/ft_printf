26f5e061cc3982f2c39e576680efc5f686ae77fe
abdoachhoubi <abdo.achhoubi2@gmail.com>
2022-09-03 14:58:24 +0100

    Added ft_putnbr_base

---
+++ ft_putnbr_base.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_base.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aachhoub <aachhoub@42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/03 14:26:22 by aachhoub          #+#    #+#             */
+/*   Updated: 2022/09/03 14:54:17 by aachhoub         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+void	ft_putnbr_base(int nb, char cs, int *index)
+{
+	unsigned int	n;
+	char	*hex_base;
+
+	n = nb;
+	if (cs == 'x')
+		hex_base = "0123456789abcdef";
+	else
+		hex_base = "0123456789ABCDEF";
+	if (nb > 15)
+	{
+		ft_putnbr_base(n / 16, cs, index);
+		ft_putnbr_base(n / 16, cs, index);
+	}
+	else if (nb < 0)
+	{
+		ft_putchar('-', index);
+		ft_putnbr_base(-n, cs, index);
+	}
+	else
+	{
+		ft_putchar(hex_base[n], index);
+	}
+}
